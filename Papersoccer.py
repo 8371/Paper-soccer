91f9fbd58f9a0dae44a695528b58624ce2b0266a
8371 <38053146+8371@users.noreply.github.com>
2018-07-22 04:41:13 +0200

    Update Papersoccer.py

--- Papersoccer.py
+++ Papersoccer.py
@@ -2,8 +2,6 @@ import pygame
 import time
 from pprint import pprint
 #stores information about movements
-#make moves with num keypad 1-9
-#undo move with key "f"
 made_movements=''
 def start():
     global center,table,board,court,position_x,position_y,o,draw,color,possible_line_directions,table,made_movements,change_color,x
@@ -29,6 +27,10 @@ def start():
     change_color=-1
     #This table... include in foto Image_1.png.............
     #so that the player does not go beyond the map
+    for i in range(2):
+        table[1][2+i]='798'
+    for i in range(2):
+        table[1][7+i]='789'
     for i in range(15):                      
         table[i][0]='471'
     for i in range(15):                      
@@ -41,36 +43,34 @@ def start():
         table[2][2+i]='46'
     for i in range(2):
         table[2][7+i]='46'
+    table[1][1]='728'
+    table[1][3]='7'
+    table[1][4]='98'
+    table[1][6]='78'
+    table[1][7]='9'
+    table[1][9]='98'
     table[2][1]='2846'
     table[2][4]='6'
     table[2][6]='4'
+    table[2][7]='46'
     table[2][9]='468'
-    table[1][7]='9'
-    table[1][3]='7'
-    for i in range(2):
-        table[1][2+i]='798'
-    table[1][4]='98'
-    table[1][1]='728'
-    for i in range(2):
-        table[1][7+i]='789'
-    table[1][6]='78'
-    table[1][9]='98'
-    table[13][2]='123'
-    table[13][1]='123'
-    table[13][3]='123'
-    table[12][2]='46'
-    table[12][3]='46'
     table[12][1]='42'
-    table[13][4]='3'
+    table[12][2]='46'
+    table[12][3]='46'    
     table[12][4]='6'
-    table[13][4]='23'
-    table[13][6]='2'
-    table[13][7]='123'
-    table[13][8]='123'
+    table[12][6]='4'
     table[12][7]='46'
     table[12][8]='46'
+    table[12][9]='62'
+    table[13][1]='123'    
+    table[13][2]='123'
+    table[13][3]='123'
+    table[13][4]='23'
     table[13][6]='12'
+    table[13][7]='123'
+    table[13][8]='123'
     table[13][9]='23'
+
     ## .....................
 
 def Result(x,x1):
